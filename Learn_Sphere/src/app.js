0d519e9332430a82fd6cbef5a73fd87bc59fe319
ros1025 <redoctobers.1025@proton.me>
2025-08-31 17:49:27 +0800

    Further Configuration to Remove Generic Files

---
+++ Learn_Sphere/src/app.js
@@ -0,0 +1,1 @@
+//TO-DO: Make Node Server and Link to Database
---
+++ Learn_Sphere/src/main.jsx
@@ -0,0 +1,9 @@
+import './style.css'
+import React from 'react'
+import { createRoot } from 'react-dom/client'
+
+createRoot(document.getElementById('app')).render(
+    <>
+    <h1>Hello World!</h1>
+    </>
+);
