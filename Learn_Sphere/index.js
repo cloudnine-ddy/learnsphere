a369473382f7aadb998859251d0c7ab3f48573c3
ros1025 <redoctobers.1025@proton.me>
2025-09-03 00:24:38 +0800

    Add Login and Register POST commands to be called by the front end. This is a basic version

--- Learn_Sphere/index.js
+++ Learn_Sphere/index.js
@@ -2,12 +2,39 @@ import express from 'express';
 import cors from 'cors';
 
 import config from './config.js';
+import { getAuth, createUserWithEmailAndPassword, signInWithEmailAndPassword } from "firebase/auth";
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 
+app.post('/login', (req, res) => {
+  const auth = getAuth();
+  signInWithEmailAndPassword(auth, req.body.email, req.body.password)
+    .then((userCredential) => {
+      res.cookie('user', userCredential);
+      res.status(200).send('login successful!');
+    })
+    .catch((error) => {
+      res.status(error.code).send(error.message);
+    })
+})
+
+app.post('/register', (req, res) => {
+  const auth = getAuth();
+  createUserWithEmailAndPassword(auth, req.body.email, req.body.password)
+    .then((userCredential) => {
+      const user = userCredential.user;
+      user.displayName = req.body.displayName;
+
+      res.cookie('user', userCredential);
+      res.status(200).send('registration successful!');
+    })
+    .catch((error) => {
+      res.status(error.code).send(error.message);
+    })
+})
 
 app.listen(config.port, () =>
   console.log(`Server is live @ ${config.hostUrl}`),
