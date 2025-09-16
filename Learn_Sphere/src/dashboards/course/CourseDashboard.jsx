f36aeee69ab811ee77c1162d93ed710f0f98bbf8
NickOoi42a <jooi0030@student.monash.edu>
2025-09-16 17:00:01 +0800

    Optimisation for CourseDashboard

--- Learn_Sphere/src/dashboards/course/CourseDashboard.jsx
+++ Learn_Sphere/src/dashboards/course/CourseDashboard.jsx
@@ -1,7 +1,7 @@
 import React from "react";
 import { useState, useEffect } from "react";
 
-import { getLessons } from "../../components/getLessons";
+import { getCourses } from "../../components/getCourses";
 
 import styles from "./CourseDashboard.module.css";
 
@@ -13,8 +13,8 @@ import AddCourseCard from "../../components/clickable/AddCourseCard";
 
 function CourseDashboard({userData}) {
     const [filter, setFilter] = useState(true);
-    const [label, setLabel] = useState("All Lessons");
-    const [lessons, setLessons] = useState([]);
+    const [label, setLabel] = useState("All courses");
+    const [courses, setCourses] = useState([]);
 
     const options = [
         {label: "All Courses", state: true},
@@ -42,12 +42,12 @@ function CourseDashboard({userData}) {
                 <div className={styles.infoTitle}>
                     My Courses
                 </div>
-                {userData != null && ( userData.role == "student" ? <FilterDropdown label={"Your Lessons"} options={[{label: "Your Lessons", state: 'Published'}]} changeEvent={changeEvent} /> : <FilterDropdown label={label} options={options} changeEvent={changeEvent} />)}
+                {userData != null && ( userData.role == "student" ? <FilterDropdown label={"Your courses"} options={[{label: "Your courses", state: 'Published'}]} changeEvent={changeEvent} /> : <FilterDropdown label={label} options={options} changeEvent={changeEvent} />)}
             </div>
             <div className={styles.infoScroll}>
                 <div className={styles.cardContainer}>
-                    {userData != null && ( userData.role != "student" ? <AddCourseCard lessonID={"Add Course"} lessonTitle={"Your Lessons"} creditPoint={0} instructorName={"Student"} href={"/home/newcourse"}/> : null)}
-                    {lessons.map((lesson) => <CourseCard key={lesson.id} lessonID={lesson.data().lessonID} lessonTitle={lesson.data().title} creditPoint={lesson.data().creditPoint} instructorName={lesson.data().owner} href={`/home/courses/wooooo`}/>)}
+                    {userData != null && ( userData.role != "student" ? <AddCourseCard courseID={"Add Course"} courseTitle={"Your courses"} creditPoint={0} instructorName={"Student"} href={"/home/newcourse"}/> : null)}
+                    {courses.map((course) => <CourseCard key={course.id} lessonID={course.data().courseID} lessonTitle={course.data().courseTitle} creditPoint={course.data().courseTotalCreditpoint} instructorName={course.data().courseSupervisor} href={`/home/courses/${course.id}`}/>)}
                 </div>
             </div>
         </>
