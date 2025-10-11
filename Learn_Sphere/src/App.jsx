dd88dff65aa8fabaa278b1b550552aeb47360c25
cloudnine-ddy <jiadon5474@gmail.com>
2025-10-11 18:44:21 +0800

    Add "Focus Mode Page" frame and GrowthSection

--- Learn_Sphere/src/App.jsx
+++ Learn_Sphere/src/App.jsx
@@ -6,6 +6,7 @@ import RegisterPage from "./pages/RegisterPage";
 import LoginPage from "./pages/LoginPage";
 import DashboardPage from "./pages/DashboardPage";
 import ByeBye from "./pages/byebye";
+import FocusPage from "./pages/FocusPage";
 
 import {BrowserRouter, Routes, Route, Navigate} from "react-router-dom";
 
@@ -20,6 +21,7 @@ function App() {
           <Route path="/login" element={<LoginPage />} />
           <Route path="/home/*" element={<DashboardPage />} />
           <Route path="/byebye" element={<ByeBye />} />
+          <Route path="/focus" element={<FocusPage totalTime="10"/>} />
         </Routes>
       </BrowserRouter>
   );
---
+++ Learn_Sphere/src/pages/FocusPage.jsx
@@ -0,0 +1,58 @@
+
+import React, { useState, useEffect } from "react";
+import Button from "../components/clickable/Button";
+import GrowthSection from "../components/functional/GrowthSection"
+import styles from "./FocusPage.module.css";
+
+function FocusPage({ totalTime = 25 * 60 }) {
+
+  
+  const [remainingTime, setRemainingTime] = useState(totalTime);
+  const [isRunning, setIsRunning] = useState(true);
+
+  useEffect(() => {
+    if (isRunning && remainingTime > 0) {
+      const timer = setInterval(() => {
+        setRemainingTime((prev) => prev - 1);
+      }, 1000);
+      return () => clearInterval(timer);
+    }
+  }, [isRunning, remainingTime]);
+
+  const progress = ((totalTime - remainingTime) / totalTime) * 100;
+
+  const formatTime = (sec) => {
+    const m = String(Math.floor(sec / 60)).padStart(2, "0");
+    const s = String(sec % 60).padStart(2, "0");
+    return `${m}:${s}`;
+  };
+
+  
+
+
+
+  return (
+    <div className={styles.container}>
+
+        <div className={styles.top}>
+            <p>Focusing</p>
+        </div>
+        <div className={styles.middle}>
+          <div className={styles.timeQuoteContainer}>
+            <p className={styles.time}>{formatTime(remainingTime)}</p>
+            <p className={styles.quote}>Live a life you will remember ⭐</p>
+          </div>
+          <div className={styles.growthSectionContainer}>
+            <GrowthSection progress={progress} />
+          </div>
+            
+        </div>
+        <div className={styles.bottom}>
+            <Button label="Give Up" type = "button" />
+        </div>
+      
+    </div>
+  );
+}
+
+export default FocusPage;
